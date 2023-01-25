10bfe5ce2765f2dc4b57ee3e75bfd218561312c7
Naim Chaya <naimchaya.dev@gmail.com>
2023-01-25 13:40:30 -0300

    Fixing Deployment-v1

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ import express from "express";
 import fileUpload from "express-fileupload";
 import postsRoutes from "./routes/posts.routes.js";
 import cors from "cors";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // middlewares
 app.use(cors());
@@ -13,10 +16,15 @@ app.use(
     useTempFiles: true,
     tempFileDir: "./upload",
   })
-);
+);Rail Deployment
 
 //routes
 
 app.use(postsRoutes);
+
+app.use(express.static(join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "../client/dist/index.html"));
+});
 
 export default app;
